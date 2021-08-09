f4ea887f1b178a712340518536c902b548f410e8
Oleksandr220 <74794729+Oleksandr220@users.noreply.github.com>
2021-08-09 20:19:14 +0300

    first deploy

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import "./App.css";
+import React, { Component } from "react";
+import Form from "./components/Form/Form";
+import Filter from "./components/Filter/Filter";
+import ContactsList from "./components/ContactsList/ContactsList";
+import Conteiner from "./components/Conteiner/Conteiner";
+import shortid from "shortid";
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+  };
+  addContacts = (data) => {
+    const contact = {
+      id: shortid.generate(),
+      name: data.name,
+      number: data.number,
+    };
+
+    const inIsContact = (e) => {
+      return contact.name === e.name;
+    };
+    if (data.name === "" || data.number === "") {
+      alert("Please make a correct contact");
+      return;
+    }
+    if (this.state.contacts.some(inIsContact)) {
+      alert(`${data.name} is already in contacts`);
+    } else
+      this.setState(({ contacts }) => ({
+        contacts: [contact, ...contacts],
+      }));
+  };
+
+  deleteContact = (eventId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== eventId),
+    }));
+  };
+
+  changeFilter = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLocaleLowerCase();
+
+    const visibleContacts = this.state.contacts.filter(
+      (contact) =>
+        contact.name.includes(normalizedFilter) ||
+        contact.number.includes(normalizedFilter)
+    );
+
+    return (
+      <>
+        <Conteiner title="Phonebook">
+          <Form onSubmit={this.addContacts} />
+        </Conteiner>
+        <Conteiner title="Contacts">
+          {contacts.length > 0 ? (
+            <>
+              <Filter value={filter} onChange={this.changeFilter} />
+              <ContactsList
+                users={visibleContacts}
+                ondeleteContact={this.deleteContact}
+              />
+            </>
+          ) : (
+            <span className="contact-message">
+              You have no contacts yet, please add contact to phoneboock :)
+            </span>
+          )}
+        </Conteiner>
+      </>
+    );
+  }
+}
+
+export default App;
